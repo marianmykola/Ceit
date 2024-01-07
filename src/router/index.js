f2fc3c5351e6d75bb8e2435d97bd3a607073a2b5
Marian <marianmykola88@gmail.com>
2024-01-07 11:49:16 +0100

    +PG

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+
+import { createWebHistory, createRouter } from 'vue-router';
+import LoginForm from '../components/LoginForm.vue';
+import DataTable from '../components/DataTable.vue';
+import RegistrationForm from '@/components/RegistrationForm.vue';
+import DataBasePG from '../components/DataBasePG.vue';
+
+
+
+const routes = [
+  {
+  path: '/',
+  component: LoginForm
+  },
+   {
+    path: '/register',
+    component: RegistrationForm,
+  },
+  { path: '/data', component: DataTable, meta: { requiresAuth: true } },
+
+  { path: '/dataPG', component: DataBasePG, meta: { requiresAuth: true } }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+  routes,
+})
+
+export default router
